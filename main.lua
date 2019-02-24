b81eed0654f8d7f0fa78868e16b85d41ccde42ce
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-02-24 12:03:10 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------
 --
--- This program you put in the diameter of the pizza 
+-- This program you put in the diameter of the pizza in inches and it will give the cost in dollars
 --
 -- Created on: 2019-02-21
 -- Created by: Aden Rao
@@ -10,26 +10,34 @@
 --Backround & Title
 display.setDefault( "background", 100/255, 155/255, 250/255 )
 
-local myText = display.newText( "Pizza Cost Calculator", 160, 5, native.systemFont, 20 )
-myText:setFillColor( 255/255, 100/255, 0 )
+local myText = display.newText( "Pizza Cost Calculator", 160, -2, native.systemFont, 30 )
+myText:setFillColor( 0/255, 0/255, 0 )
 
-local pizza = display.newImageRect( "assets/pizza.png", 150, 150 )
-pizza.x = 150
-pizza.y = 130
+local pizza = display.newImageRect( "assets/pizza.jpg", 360, 240 )
+pizza.x = display.contentCenterX
+pizza.y = 180
 
 --variables
-local diameterOfACircleTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 150, 250, 30 )
+local diameterOfACircleTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 225, 250, 30 )
 diameterOfACircleTextField.id = "diameterOfACircle textField"
-local myText = display.newText( "Diameter  ↓", 160, 350, native.systemFont, 20 )
+local myText = display.newText( "Diameter  ↓", 160, 415, native.systemFont, 20 )
 
-local calculateButton = display.newImageRect( "./assets/button.png", 75, 50 )
+local calculateButton = display.newImageRect( "./assets/button.png", 150, 50 )
 calculateButton.x = display.contentCenterX
-calculateButton.y = display.contentCenterY
+calculateButton.y = 355
 calculateButton.id = "calculate button"
 
-local priceOfPizzaTextField = display.newText( "Answer", display.contentCenterX, display.contentCenterY - 200, native.systemFont, 25 )
+local priceOfPizzaTextField = display.newText( "Answer", display.contentCenterX, display.contentCenterY - 200, native.systemFont, 20 )
 priceOfPizzaTextField.id = "priceOfPizza textField"
 priceOfPizzaTextField:setFillColor( 1, 1, 1 )
+
+function round(val, decimal)
+  if (decimal) then
+    return math.floor( (val * 10^decimal) + 0.5) / (10^decimal)
+  else
+    return math.floor(val+0.5)
+  end
+end
 
 local function calculateButtonTouch( event )
  
@@ -39,7 +47,7 @@ local function calculateButtonTouch( event )
     diameterOfACircle = diameterOfACircleTextField.text
     priceOfPizza =  (diameterOfACircle * 0.5 + 1.75) * 1.13
 
-    priceOfPizzaTextField.text = "The cost of the Pizza is " .. costOfPizza
+    priceOfPizzaTextField.text = "The cost of the Pizza is $" .. round(priceOfPizza,2)
 
     return true
 end
